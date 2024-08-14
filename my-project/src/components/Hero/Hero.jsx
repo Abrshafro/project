2a075d3d61749e3e93fe9286a917d60a3d59156f
Abrshafro <abrhammgetahun@gmail.com>
2024-08-14 12:17:14 -0700

    second commit

--- my-project/src/components/Hero/Hero.jsx
+++ my-project/src/components/Hero/Hero.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ShiroImg from '../../assets/shiro.png';
+import Navbar from '../Navbar/Navbar';
+import {FaFacebookF,FaTiktok,FaTelegram} from 
+'react-icons/fa';
 
 const Hero = () => {
+    const[sidebar,setSidebar]=React.useState(false);
   return <main className='md:px-12 md:py-6 bg-primaryDark'>
     <section className='relative min-h-[650px]
     bg-gradient-to-r from-primary to-secondary
     w-full md:rounded-xl shadow-md'>
         <div className='container'>
             {/*Navbar section*/}
+            <Navbar sidebar={sidebar} setSidebar={setSidebar}/>
 
             {/*Hero section*/}
             <div className='grid grid-cols-1
@@ -40,8 +45,45 @@ const Hero = () => {
       <h1 className='large-text'>shiro</h1>
 
       {/* sidebar section */}
+      {sidebar &&(
+        <div className='absolute top-0 right-0 
+        w-[140px] h-full bg-gradient-to-b
+        from bg-primary to-secondary z-10'>
+            <div className='w-full h-full flex
+            justify-center items-center'>
+                <div className='text-white flex
+                flex-col justify-center
+                items-center gap-6'>
+                    {/*line */}
+                    <div className='w-[1px] h-[70px]
+                    bg-white'></div>
+                    <div className='inline-block p-2
+                    rounded-full cursor-pointer
+                    border border-white'>
+                        <FaTelegram className='text-2xl'/>
+                    </div>
+                    <div className='inline-block p-2
+                    rounded-full cursor-pointer
+                    border border-white'>
+                        <FaTiktok className='text-2xl'/>
+                    </div>
+                    <div className='inline-block p-2
+                    rounded-full cursor-pointer
+                    border border-white'>
+                        <FaFacebookF className='text-2xl'/>
+                    </div>
+                     {/*line */}
+                     <div className='w-[1px] h-[70px]
+                    bg-white'></div>
 
-      
+                </div>
+
+            </div>
+
+        </div>
+      ) }
+
+
     </section>
     </main>;
   
--- my-project/src/components/Navbar/Navbar.jsx
+++ my-project/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import {GiHamburgerMenu} from "react-icons/gi";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+const Navbar = ({sidebar,setSidebar}) => {
+  return(
+   <div className='absolute top-0 left-0 w-full
+   text-white z-20'>
+    <div className='container'>
+      <div className='flex justify-between
+      items-center'>
+        {/*logo section */}
+        <h1 className='text-4xl font-bold
+        uppercase'>ab <span className='font-normal
+        text-2xl'>shiro</span>
+        </h1>
+        {/*Navlinks Section */}
+        <ul className=' lg:flex hidden space-x-4 text-xl'>
+          <li href="#">Home</li>
+          <li href="#">where to find</li>
+          <li href="#">contact</li>
+        </ul>
+        {/*Hamburger menu */}
+        <div onClick={()=>setSidebar(!sidebar)}>
+         <GiHamburgerMenu className='text-3xl
+          cursor-pointer'/>
+        </div>
+      </div>
+
+    </div>
+
+   </div>
+  );
   
 };
 
